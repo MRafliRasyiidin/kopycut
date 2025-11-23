331ed18becdab1f67805b7b4abba85f9d83c181d
Indraswara <indrswara@gmail.com>
2025-11-23 20:15:39 +0700

    feat: add audio fail and reload if hit by bullet

--- scenes/enemy/turret/bullet.gd
+++ scenes/enemy/turret/bullet.gd
@@ -16,6 +16,8 @@ func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player") or body.is_in_group("Obstacle"):
 		queue_free()
 		AudioController.play_bullet_hit()
+		get_tree().reload_current_scene()
+		AudioController.play_fail()
 		
 func set_direction(direction: Vector2):
 	self.direction = direction
@@ -25,4 +27,5 @@ func set_speed(speed: float):
 
 func _on_area_entered(area: Area2D) -> void:
 	if area.is_in_group("Obstacle"):
+		AudioController.play_bullet_hit()
 		queue_free()
