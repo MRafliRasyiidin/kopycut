79f3b2b2c760a25acb7077d3dbf37439f8bddfc2
MRafliRasyiidin <masraflirasid@gmail.com>
2025-11-23 21:33:46 +0700

    fix: main menu conflict

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,8 +1,11 @@
 extends Control
 
+func _on_play_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/level_1.tscn")
 
+func _on_exit_pressed() -> void:
+	get_tree().quit()
 
 func _on_button_button_up() -> void:
 	AudioController.play_menu_click()
 	get_tree().change_scene_to_file("res://scenes/level_1.tscn")
-	pass # Replace with function body.
--- scripts/menu.gd
+++
@@ -1,7 +0,0 @@
-extends Control
-
-func _on_play_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/level_1.tscn")
-
-func _on_exit_pressed() -> void:
-	get_tree().quit()
