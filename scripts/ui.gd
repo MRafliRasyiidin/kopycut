4a77e91e9e214ba96b0421e98994eb7d9e0a3fe5
Intermaze <rafikiprawhira@gmail.com>
2025-11-23 21:11:21 +0700

    feat: volume slider

--- scripts/ui.gd
+++ scripts/ui.gd
@@ -1,5 +1,13 @@
 extends CanvasLayer
 
+@onready var volume_slider: Control = $VolumeSlider
+@onready var music_slider: HSlider = $VolumeSlider/VBoxContainer/MusicSlider
+@onready var sfx_slider: HSlider = $VolumeSlider/VBoxContainer/SFXSlider
+
+func _ready() -> void:
+	music_slider.value = db_to_linear(AudioServer.get_bus_volume_db(2))
+	sfx_slider.value = db_to_linear(AudioServer.get_bus_volume_db(1))
+
 func _on_pause_button_up() -> void:
 	if get_tree().paused:
 		AudioController.play_menu_click()
@@ -18,12 +26,16 @@ func _on_windowed_button_up() -> void:
 	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 
 func _on_volume_button_up() -> void:
-	AudioController.play_menu_click()
-	var bus := AudioServer.get_bus_index("Master")
-	var muted := AudioServer.is_bus_mute(bus)
-	AudioServer.set_bus_mute(bus, !muted)
+	volume_slider.visible = !volume_slider.visible
 
 func _on_exit_button_up() -> void:
 	AudioController.play_menu_click()
 	get_tree().paused = false
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
+
+
+func _on_music_slider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(2, linear_to_db(value))
+
+func _on_sfx_slider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(1, linear_to_db(value))
